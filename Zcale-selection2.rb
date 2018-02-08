7f344f52f960b499b9514507fb2ffe4cfb7bf05e
KURURU <i@okako.com>
2018-02-08 21:17:32 +0900

    add context menu
    add context menu

--- Zcale-selection2.rb
+++ Zcale-selection2.rb
@@ -16,7 +16,7 @@ if sel.length > 0
   end
   mod.commit_operation; # "Zcale_selection 1 feet to 1 mm"
   Sketchup.send_action('viewZoomToSelection:')
-  prompts = ["End of Zcale_selection 1 feet to 1 mm"]  
+  prompts = ["End of Zcale 1 feet to 1 mm"]  
   UI.messagebox prompts
 end
 
@@ -26,5 +26,10 @@ end; # module Zcale_selection2
 if not file_loaded?(__FILE__)
 #	UI.menu("Draw").add_separator 
 	UI.menu("Draw").add_item("Zcale 1 feet to 1 mm") { Zcale_selection2.main }
+	
+	UI.add_context_menu_handler do |context_menu|
+	  context_menu.add_item("Zcale 1 feet to 1 mm") {Zcale_selection2.main}
+	end
+
 	file_loaded(__FILE__)
 end
